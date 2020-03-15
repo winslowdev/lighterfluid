bdec49f92ba9f554195419f35df2eebde7ab4a33
Addison <49768250+winslowdev@users.noreply.github.com>
2020-03-15 14:50:31 -0400

    working dev

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Homepage } from './components/Homepage.js'
+import { Developers }  from './components/Developers.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+        this.navigate = this.navigate.bind(this)
+    }
+
+    navigate(type, address) {
+        if (type === "external") {
+            window.open(address)
+        } else if (type === "internal") {
+            window.location.href = address
+        }
+    }
+
+    render() {
+        return (
+            <Router>
+                <Route
+                    path="/" exact
+                    render={(props) => <Homepage {...props} navigate={this.navigate} />} />
+
+                <Route
+                    path="/developers" exact
+                    render={(props) => <Developers {...props} navigate={this.navigate} />} />
+            </Router>
+        )
+    }
 }
-
-export default App;
